ca3eba5ea29b45175b0c072a95b1edb4cf76aa37
stefan-hennings <stefan.hennings4@gmail.com>
2021-06-06 19:15:47 +0200

    Restructuring

--- src/main/java/TransactionController.java
+++ src/main/java/TransactionController.java
@@ -7,33 +7,41 @@ public class TransactionController {
     private final Bank bank;
     
     public void attemptWithdrawal(int amountToWithdraw) {
-        Withdrawal withdrawal = validateWithdrawal(amountToWithdraw);
+        Withdrawal withdrawal = new Withdrawal(amountToWithdraw);
+        validateWithdrawalAndSetBillTypes(withdrawal);
         processWithdrawal(withdrawal);
     }
     
-    public Withdrawal validateWithdrawal(int amountToWithdraw) {
-        Withdrawal withdrawal = new Withdrawal(amountToWithdraw);
-        if (amountToWithdraw <= 0 || amountToWithdraw % 100 != 0) {
-            return withdrawal.decline("Invalid withdrawal amount. Must be a positive integer divisible by 100. ");
-        } else if (amountToWithdraw > bank.getTotalValue()) {
-            return withdrawal.decline("Funds too low. Try a lower amount. ");
+    public void validateWithdrawalAndSetBillTypes(Withdrawal withdrawal) {
+        if (!validateWithdrawalAmount(withdrawal)) {
+            return;
         }
-    
-        amountToWithdraw = decideBillTypes(amountToWithdraw, withdrawal);
-    
-        if (amountToWithdraw != 0) {
-            return withdrawal.decline("Not enough bills of the correct types to complete withdrawal. Try a different amount. ");
+        
+        int amountRemaining = decideBillTypes(withdrawal.getAmount(), withdrawal);
+        
+        if (amountRemaining != 0) {
+            withdrawal.decline("Not enough bills of the correct types to complete withdrawal. Try a different amount. ");
         } else {
             withdrawal.setApproved(true);
         }
-        
-        return withdrawal;
+    }
+    
+    private boolean validateWithdrawalAmount(Withdrawal withdrawal) {
+        if (withdrawal.getAmount() <= 0 || withdrawal.getAmount() % 100 != 0) {
+            withdrawal.decline("Invalid withdrawal amount. Must be a positive integer divisible by 100. ");
+            return false;
+        } else if (withdrawal.getAmount() > bank.getTotalValue()) {
+            withdrawal.decline("Funds too low. Try a lower amount. ");
+            return false;
+        }
+        return true;
     }
     
     private int decideBillTypes(int amountToWithdraw, Withdrawal withdrawal) {
         for (Integer denomination : bank.getSortedKeysDescending()) {
             int billsOfCurrentType = Integer.min(amountToWithdraw / denomination, bank.getBillQuantityOfValue(denomination));
             amountToWithdraw -= billsOfCurrentType * denomination;
+            
             withdrawal.addBillType(denomination, billsOfCurrentType);
         }
         return amountToWithdraw;
